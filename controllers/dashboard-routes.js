0930b221822b7e9a667781e32025978133698efc
Tricia <tricianghiem@gmail.com>
2022-09-15 21:56:18 -0500

    Routes to get all post

---
+++ controllers/dashboard-routes.js
@@ -0,0 +1,50 @@
+const router = require("express").Router();
+const { Post, User } = require("../models/");
+const withAuth = require("../utils/auth");
+
+// dashboard with all post
+router.get("/", withAuth, async (req, res) => {
+  try {
+    const postData = await Post.findAll({
+      where: { userId: req.session.userId },
+      include: [User],
+    });
+    const posts = postData.map((post) => post.get({ plain: true }));
+    console.log(posts);
+    res.render("all-posts", {
+      layout: "dashboard",
+      posts,
+    });
+  } catch (err) {
+    res.redirect("login");
+  }
+});
+
+// experience after creating a new post
+router.get("/new", withAuth, (req, res) => {
+  res.render("new-post", {
+    layout: "dashboard",
+  });
+});
+
+// experience after clicking a post
+router.get("/edit/:id", withAuth, async (req, res) => {
+  try {
+    const postData = await Post.findByPk(req.params.id);
+
+    if (postData) {
+      const post = postData.get({ plain: true });
+      console.log(post);
+      res.render("edit-post", {
+        layout: "dashboard",
+        post,
+      });
+    } else {
+      res.status(404).end();
+    }
+  } catch (err) {
+    res.redirect("login");
+  }
+});
+
+module.exports = router;
