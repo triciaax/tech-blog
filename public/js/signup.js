db1dcf3fc353a2f4e0329596e79579c87e9375e1
Tricia <tricianghiem@gmail.com>
2022-09-15 22:00:54 -0500

    create js for login, logout, and sign up

---
+++ public/js/signup.js
@@ -0,0 +1,29 @@
+const signupFormHandler = async function (event) {
+  event.preventDefault();
+
+  const username = document
+    .querySelector("#username-input-signup")
+    .value.trim();
+  const password = document
+    .querySelector("#password-input-signup")
+    .value.trim();
+
+  const response = await fetch("/api/user", {
+    method: "POST",
+    body: JSON.stringify({
+      username,
+      password,
+    }),
+    headers: { "Content-Type": "application/json" },
+  });
+
+  if (response.ok) {
+    document.location.replace("/dashboard");
+  } else {
+    alert("Failed to sign up");
+  }
+};
+
+document
+  .querySelector(".signup-form")
+  .addEventListener("submit", signupFormHandler);
